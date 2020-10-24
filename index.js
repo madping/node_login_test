0c6f0fbd6b5a0416377121a16d65207a8eaa11b0
madping <otaru007x@gmail.com>
2020-10-24 21:27:41 +0900

    .

--- index.js
+++ index.js
@@ -5,42 +5,48 @@ require('dotenv').config();
 const express = require('express')
 const app = express()
 const port = 5000
+
+const {User} = require("./models/User");
+
+const bodyParser = require('body-parser');
+
 const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URL,
-
+const bodyParser = require('body-parser');
 
 
 
-        {
+mongoose.connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+}).then(() => console.log("hello connected...."))
+        .catch(err => console.log(err));
 
 
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-                useCreateIndex: true,
-                useFindAndModify: false
-        },
-
-
-
-
-
-        (err) => {
-
-
-
-
-                if (err) {
-                        console.log("db err", err);
-                } else {
-                        console.log("db connected");
-
-                }
-        });
+//application/x-www-firm-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json
+app.use(bodyParser.json());
 
 
 
 app.get('/', (req, res) => {res.send('hello han')})
+
+
+app.post('/register', (req, res) => {
+        //회원 가입에 필요한 정보들을 클라이언트에서 가져오면
+        //그것들을 디비에 넣어준다.
+        const user = new User(req.body)
+
+        user.save((err, doc) => {
+                if (err) return res.json({success: false, err})
+
+                return res.status(200), json({success: true})
+        })
+})
+
+
 
 app.listen(port, () => console.log(`youre port ${port}`))
 
